0bf593c8f79088567dc054504aa453f12b0d8f54
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-11 16:05:54 -0300

    Criando a pagina para edição de Categorias e editando a rota ao criar uma nova categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -9,7 +9,7 @@ router.get("/admin/categories/new",(req,res)=>{
     res.render("admin/categories/new");
 });
 
-
+//SALVAR CATEGORIAS
 router.post("/categories/save",(req,res)=>{
     var title = req.body.title;
         if(title != undefined){
@@ -18,7 +18,7 @@ router.post("/categories/save",(req,res)=>{
                 title: title,
                 slug: slugify(title)
             }).then(()=> {
-                res.redirect("/")
+                res.redirect("/admin/categories")
             })
 
         }else{
@@ -54,6 +54,25 @@ router.post("/categories/delete", (req,res) =>{
         res.redirect("admin/categories")
     }
 });
+//EDIATANDO
+router.get("/admin/categories/edit/:id", (req,res)=>{
+    var id = req.params.id;
 
+     if(isNaN(id)){
+         res.redirect("/admin/categories");
+     }
+    Category.findByPk(id).then(category =>{
+        if(category != undefined){
+
+            res.render("admin/categories/edit",{category: category});
+
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+});
 
 module.exports = router;
