acf2a477dbcd1853fd36ea4ba61adf7ccfc8aad5
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-14 14:38:09 -0300

    adicionando TextArea TinyMCE ao formulario de artigo e listando as categorias ao formulario

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
+//MODEL DE CATEGORIA
+const Category = require("../categories/Category");
+
 
 router.get("/articles",(req,res)=>{
     res.send("RODA DE ARTIGOS")
 });
 router.get("/admin/articles/new",(req,res)=>{
-    res.send("ROTA PARA CRIAR NOVO ARTIGO")
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories: categories})
+    })
+   
+
+
+
 });
+
+
+
+
 
 module.exports = router;
