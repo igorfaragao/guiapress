2719317b2ad5984fd7babad5b8817fe1fc6db352
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-16 09:35:36 -0300

    Criando Menu dinâmico na barra de navegação

--- index.js
+++ index.js
@@ -46,7 +46,14 @@ app.get("/", (req,res) =>{
             ['id','DESC']
         ]
     }).then(articles =>{
-        res.render("index",{articles: articles});
+
+        Category.findAll().then(categories=>{
+
+            
+        res.render("index",{articles: articles, categories: categories});
+
+        });
+    
     });
 
 });
@@ -61,7 +68,12 @@ app.get("/:slug",(req,res)=>{
         }
     }).then(article =>{
         if(article != undefined){
-            res.render("article",{article: article});
+            Category.findAll().then(categories=>{
+
+            
+                res.render("article",{article: article, categories: categories});
+        
+                });
         }else{
             res.redirect("/");
         }
