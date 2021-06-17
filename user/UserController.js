c2ba9f0f9998527b90236c9647e2e489172a9c3b
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-17 14:10:51 -0300

    criando Formulário de cadastro

--- user/UserController.js
+++ user/UserController.js
@@ -10,7 +10,14 @@ router.get("/admin/users/create",(req,res)=>{
     res.render("admin/users/create")
 });
 
+router.post("/users/create",(req,res)=>{
 
+    var email = req.body.email;
+    var password = req.body.password
+
+    res.json({email,password})
+
+})
 
 
 
