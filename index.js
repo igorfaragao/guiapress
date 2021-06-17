4fc14a8290088330518c5f7ed69435c685ba296e
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-17 10:34:49 -0300

    Filtrando artigos pelas categorias da barra de navegação

--- index.js
+++ index.js
@@ -84,6 +84,30 @@ app.get("/:slug",(req,res)=>{
 
 });
 
+
+    app.get("/category/:slug",(req,res)=>{
+        var slug = req.params.slug;
+        Category.findOne({
+            where:{
+                slug:slug
+            },include:[{model: Article}]
+        }).then(category =>{
+            if(category != undefined){
+
+                Category.findAll().then(categories =>{
+                    res.render("index",{articles: category.articles, categories: categories})
+                });
+
+            }else{
+                res.redirect("/")
+            }
+        }).catch(err =>{
+            res.redirect("/")
+        });
+    });
+
+
+
 app.listen(4040,() =>{
     console.log("O servidor está rodando!")
 });
