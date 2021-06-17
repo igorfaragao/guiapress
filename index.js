d3344f0eab75e0ac3a4cb26b1729d4e3763488ce
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-06-17 13:37:24 -0300

    Finalizando o sistema de paginação de artigos

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ app.get("/", (req,res) =>{
     Article.findAll({
         order:[
             ['id','DESC']
-        ]
+        ],
+        limit: 4
     }).then(articles =>{
 
         Category.findAll().then(categories=>{
